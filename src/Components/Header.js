c3b9c4476762c962021466703794bd5f2b2251b0
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-08 15:51:12 -0800

    Finish top-bar and stylized with CSS

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+function Header(){
+    return(
+        <header>
+            <div className = 'top-bar'>
+                <div className = "logo"></div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
