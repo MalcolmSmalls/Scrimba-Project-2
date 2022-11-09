0802d4a149898051805aeb60637de54cb7a85472
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-08 17:57:46 -0800

    Divide header component into another component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Header'
+import Hero from './Components/Hero'
 
 function App() {
   return (
    <div>
-    <Header />
+     <Header />
+      <Hero />
    </div>
   );
 }
---
+++ src/Components/Hero.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+
+function Hero(){
+    return (
+        <div className = "hero">
+        <div className = "photo-div"> </div>
+        <div className="header-bottom">
+          <h1>Online Experiences</h1>
+            <p>
+          Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+          </p>
+        </div>  
+    </div>      
+    )
+}
+
+export default Hero
