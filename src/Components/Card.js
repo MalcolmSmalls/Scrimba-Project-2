def7abed2a6593cd2696e0598556aa3d5532d664
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-11 10:45:06 -0800

    Shortened props list and updated passed props to use the data

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,24 +1,24 @@
 import React, { Composite } from "react"
 import data from "../data.js"
 
-function Card(props){
+function Card({props}){
     let badgeText
-    if(props.openSpots === 0){
+    if(props.item.openSpots === 0){
         badgeText = "Sold Out"
-    }else if(props.country === "Online"){
+    }else if(props.item.location === "Online"){
         badgeText = "Online"
     }
     return (
         <div className = "card-container">
-            <div className = "photo-container" style={{ backgroundImage: `url('${props.img}')`}}>
-                {props.openSpots === 0 || props.country === 'Online' ? <button className = "card-btn">{badgeText}</button> : null}
+            <div className = "photo-container" style={{ backgroundImage: `url('${props.item.coverImg}')`}}>
+                {props.item.openSpots === 0 || props.item.location === 'Online' ? <button className = "card-btn">{badgeText}</button> : null}
             </div>
             <div className = "summary-container">
                 <span className = "star"><i className="fa-solid fa-star"></i></span>
-                <span className = "rating">{props.rating}</span>
-                <span className = "reviews">({props.reviewCount}) <i className="fa-solid fa-circle"></i> {props.country}</span>
-                <h2 className = "title">{props.title}</h2>
-                <p className = "price"><span>From ${props.price}</span> / person</p>
+                <span className = "rating">{props.item.stats.rating}</span>
+                <span className = "reviews">({props.item.stats.reviewCount}) <i className="fa-solid fa-circle"></i> {props.country}</span>
+                <h2 className = "title">{props.item.title}</h2>
+                <p className = "price"><span>From ${props.item.price}</span> / person</p>
             </div>
 
         </div>
