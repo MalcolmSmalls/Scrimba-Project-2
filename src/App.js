658750759063f8f3a5ff8270cd9219c7194f0d1f
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-11 09:41:06 -0800

    Map through array of data to fill components for experiences

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 import Header from './Components/Header'
 import Hero from './Components/Hero'
 import Card from './Components/Card'
+import data from './data.js'
 
 function App() {
+  const dataArr = data.map(item => {
+    return (
+      <Card img = {item.coverImg} rating = {item.stats.rating} reviewCount = {item.stats.reviewCount} country = {item.location} title = {item.title} price = {item.price} />
+    )
+  })
+
   return (
    <div>
-     <Header />
-      <Hero />
-      <Card img = './x.jpg' rating = {5.0} reviewCount = {6} country = "USA" title = "Life lessons with Katie Zaferes" price = {136}/>
+    <Header />
+    <Hero />
+    {dataArr}
    </div>
   );
 }
