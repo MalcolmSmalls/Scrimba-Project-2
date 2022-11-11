8e7513582cb1546cee8dc3fa2f87c3f0065645ca
MalcolmSmalls <smallsbeats@gmail.com>
2022-11-11 10:25:40 -0800

    Use ternary to display online badge if online

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,10 +2,16 @@ import React, { Composite } from "react"
 import data from "../data.js"
 
 function Card(props){
+    let badgeText
+    if(props.openSpots === 0){
+        badgeText = "Sold Out"
+    }else if(props.country === "Online"){
+        badgeText = "Online"
+    }
     return (
         <div className = "card-container">
             <div className = "photo-container" style={{ backgroundImage: `url('${props.img}')`}}>
-                {props.openSpots === 0 && <button className = "card-btn">Sold Out</button>}
+                {props.openSpots === 0 || props.country === 'Online' ? <button className = "card-btn">{badgeText}</button> : null}
             </div>
             <div className = "summary-container">
                 <span className = "star"><i className="fa-solid fa-star"></i></span>
